d86bb3e0e9c3a348221958528f91d4643f332a22
Zee Mai <zihao_mai@hotmail.com>
2020-06-03 15:18:10 +0100

    Update Icon

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import './Join.css';
+import signInIcon from '../../icons/signInIcon.png';
 
 export default function SignIn() {
   const [name, setName] = useState('');
@@ -18,7 +19,7 @@ export default function SignIn() {
           <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+          <button className={'button mt-20'} type="submit"><img alt="Sign in Icon" src={signInIcon}/></button>
         </Link>
       </div>
     </div>
